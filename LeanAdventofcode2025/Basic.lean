3190d4e55a78e0768afe3231714d9d9024a45ba7
Nicolas Grislain <nicolas.grislain@gmail.com>
2025-12-02 11:25:29 +0100

    ok

--- LeanAdventofcode2025/Basic.lean
+++ LeanAdventofcode2025/Basic.lean
@@ -12,23 +12,14 @@ def listTestRepsonses: IO Unit := do
   return Unit.unit
 
 def listRepsonses: IO Unit := do
-  IO.println r" ____              _
-|  _ \  __ _ _   _/ |
-| | | |/ _` | | | | |
-| |_| | (_| | |_| | |
-|____/ \__,_|\__, |_|
-             |___/   "
+  IO.println "  ____              _ \n |  _ \\  __ _ _   _/ |\n | | | |/ _` | | | | |\n | |_| | (_| | |_| | |\n |____/ \\__,_|\\__, |_|\n              |___/   "
   let input <- Day1.readInput
   IO.println "Part 1:"
   Day1.response1 input;
   IO.println "Part 2:"
   Day1.response2 input;
-  IO.println r" ____              ____
-|  _ \  __ _ _   _|___ \
-| | | |/ _` | | | | __) |
-| |_| | (_| | |_| |/ __/
-|____/ \__,_|\__, |_____|
-             |___/       "
+  IO.println ""
+  IO.println " ____              ____  \n|  _ \\  __ _ _   _|___ \\ \n| | | |/ _` | | | | __) |\n| |_| | (_| | |_| |/ __/ \n|____/ \\__,_|\\__, |_____|\n             |___/       "
   let input <- Day2.readInput
   IO.println "Part 1:"
   Day2.response1 input;
